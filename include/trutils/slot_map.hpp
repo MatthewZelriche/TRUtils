73f3af6a8336b338cc6d81f092022395ecfe0b43
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2026-03-15 16:21:45 -0600

    Add slot_map

---
+++ include/trutils/slot_map.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <span>
+
+#include "sparse_set.hpp"
+
+namespace tr {
+
+template<typename Key, typename Value, template<typename Value> class Storage = std::vector>
+class SlotMap {
+  public:
+   [[nodiscard]] Key insert(Value &&value) {
+      auto key = mMapping.insert();
+      mStorage.push_back(std::move(value));
+      return key;
+   }
+
+   [[nodiscard]] Key insert(const Value &value) {
+      auto key = mMapping.insert();
+      mStorage.push_back(value);
+      return key;
+   }
+
+   bool contains(Key key) const { return mMapping.contains(key); }
+
+   Value *get(Key key) {
+      if (!mMapping.contains(key)) { return nullptr; }
+      return &mStorage.at(mMapping.get(key));
+   }
+
+   const Value *get(Key key) const {
+      if (!mMapping.contains(key)) { return nullptr; }
+      return &mStorage.at(mMapping.get(key));
+   }
+
+   Value remove(Key key) {
+      if (!mMapping.contains(key)) { throw std::runtime_error(""); }
+
+      auto storageIdx = mMapping.get(key);
+      mMapping.erase(key);
+
+      // Swap and pop the data
+      auto oldDataSize = mStorage.size();
+      if (oldDataSize > 1) { std::swap(mStorage[storageIdx], mStorage[oldDataSize - 1]); }
+      auto val = std::move(mStorage[oldDataSize - 1]);
+      mStorage.pop_back();
+      return val;
+   }
+
+   size_t size() const { return mStorage.size(); }
+
+   void clear() {
+      mStorage.clear();
+      mMapping.clear();
+   }
+
+   Storage<Value>::iterator begin() { return mStorage.begin(); }
+   Storage<Value>::iterator end() { return mStorage.end(); }
+   Storage<Value>::const_iterator begin() const { return mStorage.begin(); }
+   Storage<Value>::const_iterator end() const { return mStorage.end(); }
+
+   std::span<Value> data() { return std::span(mStorage); }
+   std::span<const Value> data() const { return std::span(mStorage); }
+
+  private:
+   SparseSet<Key> mMapping;
+   Storage<Value> mStorage {};
+};
+
+} // namespace tr
--- tests/basic.cpp
+++
@@ -1,6 +0,0 @@
-// NOLINTBEGIN
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Basic Sanity Test", "[Basic]") {}
-
-// NOLINTEND
