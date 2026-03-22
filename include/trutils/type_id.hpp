dcecb8d6bfbce61c465dab78f887ffafe624b905
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2026-03-21 23:05:56 -0600

    fixes to type_id

--- include/trutils/type_id.hpp
+++ include/trutils/type_id.hpp
@@ -15,12 +15,12 @@ struct ty_info {
 };
 
 template<typename T>
-consteval std::string_view getUniqueTypeName() {
+consteval std::string_view get_unique_type_name() {
    std::string_view result = std::source_location::current().function_name();
    size_t pos = 0;
    size_t end = result.size();
 #ifdef _MSC_VER
-   pos = result.find("id<") + 3;
+   pos = result.find("name<") + 5;
    end = result.find(">", pos);
 #elif defined(__GNUC__) && !defined(__clang__)
    pos = result.find("T = ") + 4;
@@ -47,13 +47,13 @@ consteval uint64_t hash_string(std::string_view str) {
 
 template<typename T>
 consteval uint64_t getTypeID() {
-   return hash_string(getUniqueTypeName<T>());
+   return hash_string(get_unique_type_name<T>());
 }
 
 template<typename T>
 consteval ty_info getTypeInfo() {
    ty_info info;
-   info.name = getUniqueTypeName<T>();
+   info.name = get_unique_type_name<T>();
    info.size = sizeof(T);
    info.alignment = alignof(T);
    info.id = hash_string(info.name);
